263ed47d2f7126253d66f3e20c331ae45fa2ae50
Ahmed <ahmedjader837@gmail.com>
2025-01-11 13:55:34 -0500

    added cohere component and integrated into page.tsx, gonna implement the frontend api logic to interact and make post requests.

---
+++ src/app/components/cohere.jsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const cohere = () => {
+  return (
+    <div>cohere</div>
+  )
+}
+
+export default cohere
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Login from "@/app/components/login";
+import Cohere from "@/app/components/cohere";
 export default function Home() {
   return (
     <main className="antialiased overflow-x-hidden max-w-7xl mx-auto relative z-10">
       <Login />
+      <Cohere />
     </main>
   );
 }
